51309628389bc925e0c968eea75e427fa6dcbbf3
Joao Souza <joao.souza@gerencianet.com.br>
2022-07-19 11:38:49 +0000

    refactor: move sucess http response to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -8,6 +8,7 @@
 // Syntax Sugar
 import { ServerError } from '../erros/server-error'
 import { HttpResponse } from '../protocols/http'
+
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
@@ -15,4 +16,8 @@ export const badRequest = (error: Error): HttpResponse => ({
 export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
+})
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
 })
